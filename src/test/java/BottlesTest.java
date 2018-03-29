3b4eda2751b617b8a395a8aaa612e608d022ea4e
LeonardoBenassi97 <leonardobenassi.97@libero.it>
2018-03-29 15:08:19 +0200

    aggiunto il contenuto al metodo BottlesTest.testSong con variabile estratta

--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -1,10 +1,18 @@
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class BottlesTest
 {
     @Test
     public void testSong()
     {
-        return;
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Bottles.writeLyrics(
+                new PrintStream(
+                        byteArrayOutputStream
+                )
+        );
     }
 }
